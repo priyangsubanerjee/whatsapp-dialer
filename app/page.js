e2d15b421e941afb5c2d0378b42958d557d8a4a8
Priyangsu Banerjee <priyangsu26@gmail.com>
2025-04-14 16:25:09 +0530

    Adjust logo size for improved visual consistency in the WhatsApp Dialer interface

--- app/page.js
+++ app/page.js
@@ -29,7 +29,7 @@ export default function Home() {
   return (
     <div className="bg-[#21c063] h-svh fixed inset-0">
       <div className="mt-16 px-12">
-        <img className="w-20 mx-auto" src="https://www.cdnlogo.com/logos/w/29/whatsapp-icon.svg" alt="" />
+        <img className="w-16 mx-auto" src="https://www.cdnlogo.com/logos/w/29/whatsapp-icon.svg" alt="" />
         <h2 className="text-3xl text-white font-semibold text-center mt-6">WhatsApp Dialer</h2>
         <p className="text-sm text-white textpre text-center font-medium leading-6 mt-2">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
       </div>
