44c1fa3f230e0b62f2d390db4038e2113b420b5c
Priyangsu Banerjee <priyangsu26@gmail.com>
2025-04-14 19:53:55 +0530

    Add border to DialCard for improved visual distinction

--- app/components/DialCard.js
+++ app/components/DialCard.js
@@ -98,7 +98,7 @@ function DialCard() {
 
   return (
     <div className="p-4 fixed bottom-0 md:bottom-10 inset-x-0">
-      <div className="bg-white w-full py-6 px-5 rounded-[24px] max-w-sm mx-auto">
+      <div className="bg-white border-2 w-full py-6 px-5 rounded-[24px] max-w-sm mx-auto">
         <div className="flex items-center justify-center gap-10 max-w-[200px] mx-auto">
           {tabs.map((tab, index) => (
             <button key={index} onClick={() => setSelectedTab(index)} className={`${selectedTab === index ? "text-[#1b1b1b]" : "text-neutral-400"}`}>
