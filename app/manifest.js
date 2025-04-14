ca761d686299ad4c2bf6c7795a7397a4c3002b00
Priyangsu Banerjee <priyangsu26@gmail.com>
2025-04-14 19:57:47 +0530

    Add manifest file for WhatsApp Dialer with app metadata and icons

---
+++ app/manifest.js
@@ -0,0 +1,23 @@
+export default function manifest() {
+  return {
+    name: "WhatsApp Dialer",
+    short_name: "Dialer",
+    description: "WhatsApp Dialer",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#ffffff",
+    theme_color: "#21c063",
+    icons: [
+      {
+        src: "/icon-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/icon-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+  };
+}
