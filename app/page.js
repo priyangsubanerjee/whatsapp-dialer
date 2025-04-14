0c7a7b0f4a1f0bf81c62e4c0236c53ca53058c5a
Priyangsu Banerjee <priyangsu26@gmail.com>
2025-04-14 16:29:01 +0530

    Update font weight for header in WhatsApp Dialer for improved emphasis

--- app/page.js
+++ app/page.js
@@ -30,7 +30,7 @@ export default function Home() {
     <div className="bg-[#21c063] h-svh fixed inset-0">
       <div className="mt-16 px-12">
         <img className="w-16 mx-auto" src="https://www.cdnlogo.com/logos/w/29/whatsapp-icon.svg" alt="" />
-        <h2 className="text-3xl text-white font-semibold text-center mt-6">WhatsApp Dialer</h2>
+        <h2 className="text-3xl text-white font-bold text-center mt-6">WhatsApp Dialer</h2>
         <p className="text-sm text-white textpre text-center font-medium leading-6 mt-2">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
       </div>
       <div className="p-4 fixed bottom-0 inset-x-0">
