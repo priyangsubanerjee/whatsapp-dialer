cba98f52241cc1accf50754ca15052cebd7f9fbf
Priyangsu Banerjee <priyangsu26@gmail.com>
2025-04-14 20:03:31 +0530

    Update theme color and enhance background gradient for improved visual appeal

--- app/layout.js
+++ app/layout.js
@@ -21,7 +21,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <meta name="theme-color" content="#21c063" />
+        <meta name="theme-color" content="#fff" />
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
